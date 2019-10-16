a4049a7d2233c28affe21266957866a067ee80fb
Solace <545058656@qq.com>
2019-10-16 17:30:18 +0800

    1

---
+++ nacos-examples-master/nacos-spring-boot-example/nacos-spring-boot-config-multi-data-ids-example/src/main/java/com/alibaba/nacos/example/spring/boot/controller/ConfigController.java
@@ -0,0 +1,29 @@
+package com.alibaba.nacos.example.spring.boot.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+@RequestMapping("config")
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache}", autoRefreshed = true)
+    private boolean useLocalCache;
+    @NacosValue(value = "${userName}", autoRefreshed = true)
+    private String userName;
+    @NacosValue(value = "${zookeeper.zkAddress}", autoRefreshed = true)
+    private String zkAddress;
+
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        System.out.println(userName);
+        System.out.println(zkAddress);
+        return useLocalCache;
+    }
+}
